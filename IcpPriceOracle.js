5d11ad6a9a64f14c61efa1211f44999ca222e405
SwaichSaheb <77071009+SwaichSaheb@users.noreply.github.com>
2021-07-03 10:46:05 -0700

    added caller/contracts & oracle/contracts (#2)

---
+++ IcpPriceOracle.js
@@ -0,0 +1,10 @@
+const axios = require('axios')
+const BN = require('bn.js')
+const common = require('./utils/common.js')
+const SLEEP_INTERVAL = process.env.SLEEP_INTERVAL || 2000
+const PRIVATE_KEY_FILE_NAME = process.env.PRIVATE_KEY_FILE_NAME || './oracle/oracle_private_key'
+const CHUNK_SIZE = process.env.CHUNK_SIZE || 3
+const MAX_RETRIES = process.env.MAX_RETRIES || 5
+const OracleJSON = require('./oracle/build/contracts/IcpPriceOracle.json')
+var pendingRequests = []
+
